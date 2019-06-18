211025121620a9084996e8cb1e6b1e5c795a6bcf
ProticM <protic023@gmail.com>
2019-06-18 22:45:08 +0200

    installed font-awesome icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { AboutComponent } from './sidebar/about/about.component';
 import { ContactComponent } from './sidebar/contact/contact.component';
 import { ContentComponent } from './content/content.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,14 @@ import { ContentComponent } from './content/content.component';
     ContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+   constructor(){
+      library.add(faCoffee);
+   }
+}
