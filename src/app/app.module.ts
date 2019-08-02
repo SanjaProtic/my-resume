23e8fe4f4f0cc05e07eb0f7122b9872498eb7884
SanjaProtic <gardinovacki023@gmail.com>
2019-08-02 22:54:05 +0200

    added social links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas, faPhoneAlt, faCity, faEnvelope, faBookOpen, faCode, faMagic, faRunning} from '@fortawesome/free-solid-svg-icons';
 import { far,faFileCode } from '@fortawesome/free-regular-svg-icons';
-import { fab, faGithub, faSkype } from '@fortawesome/free-brands-svg-icons';
+import { fab, faGithub, faSkype, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -52,6 +52,7 @@ export class AppModule {
                   faFileCode,
                   faCode,
                   faMagic,
-                  faRunning);
+                  faRunning,
+                  faLinkedin);
       }
 }
